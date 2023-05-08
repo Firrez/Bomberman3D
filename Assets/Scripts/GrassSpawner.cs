0a7c4114e980abddfee8a8fc1415000f19ad7226
FreRas <fre@usekeyhole.com>
2023-05-08 18:48:13 +0200

    Grass added

---
+++ Assets/Scripts/GrassSpawner.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GrassSpawner : MonoBehaviour
+{
+    public GameObject grass;
+    
+    private const float StartZ = -69.5f;
+    private const float StartX = -49.5f;
+
+    private void Start()
+    {
+        for (var x = StartX; x < 60.5f; x++)
+        {
+            for (var z = StartZ; z < 80.5f; z++)
+            {
+                var xOffset = Random.Range(-0.3f, 0.3f);
+                var zOffset = Random.Range(-0.3f, 0.3f);
+                var pos = new Vector3(x + xOffset, 0, z + zOffset);
+                Instantiate(grass, pos, grass.transform.rotation);
+            }
+        }
+    }
+}
--- Assets/Scripts/PowerDropper.cs
+++ Assets/Scripts/PowerDropper.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PowerDropper : MonoBehaviour
 {
@@ -7,18 +9,22 @@ public class PowerDropper : MonoBehaviour
     public GameObject flame;
     public GameObject skate;
     public GameObject slow;
+    public GameObject spew;
+    public GameObject kick;
 
     private bool _hasDropped;
     
     private GameObject GetPowerUp()
     {
-        var number = Random.Range(0, 10);
+        var number = Random.Range(0, 15);
         return number switch
         {
-            0 or 1 => bomb,
-            2 or 3 => flame,
-            4 => skate,
-            5 => slow,
+            0 or 1 or 2 => bomb,
+            3 or 4 or 5 => flame,
+            6 => skate,
+            7 => slow,
+            8 => spew,
+            9 or 10 => kick,
             _ => null
         };
     }
